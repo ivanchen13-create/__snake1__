1922ba4b2ace130207395be68b42d451b7f65009
ivanchen13-create <ivanchen13@gmail.com>
2026-05-19 06:54:23 +0000

    第一次修改

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/__snake1__/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
